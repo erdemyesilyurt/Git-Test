9e3a3fb5ab43e621fc44d6eaee27bcf5dd612df3
Erdem Yeşilyurt <erdemyesilyurt94@gmail.com>
2022-11-15 15:29:34 +0300

    Git test- first commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by erdem on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        print("git")
+        print("git2")
+        
+    }
+
+
+}
+
